c0ef1a709abe84d169c09d56d8f4dc4b8e0b0fb9
kianfay <kifay@tcd.ie>
2022-03-28 13:54:45 +0100

    fix

--- src/trust_score_generators/data_types/messages/signatures/interaction_sig.rs
+++ src/trust_score_generators/data_types/messages/signatures/interaction_sig.rs
@@ -16,9 +16,10 @@ use serde::{Deserialize, Serialize};
 // contains the data being signed
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct InteractionPreSig {
-    pub contract: Vec<u8>,
+    pub contract: Contract,
     pub signer_channel_pubkey: String,
-    pub wit_node_sigs: Vec<u8>,
+    pub witnesses: WitnessClients,
+    pub wit_node_sigs: ArrayOfWnSignituresBytes,
     pub org_cert: OrgCert,
     pub timeout: u32,
 }
@@ -26,19 +27,13 @@ pub struct InteractionPreSig {
 // contains the data and a signature, as well the the key to verify with
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct InteractionSig {
-    /// Hash of the contract
-    pub contract: Vec<u8>,
-    /// Signer channel public key
+    pub contract: Contract,
     pub signer_channel_pubkey: String,
-    /// Hash of the combined witness sigs
-    pub wit_node_sigs: Vec<u8>,
-    // Organization certificate
+    pub witnesses: WitnessClients,
+    pub wit_node_sigs: ArrayOfWnSignituresBytes,
     pub org_cert: OrgCert,
-    // The timeout for the interaction
     pub timeout: u32,
-    // The signer's DID public key
     pub signer_did_pubkey: String,
-    // Signature for this information
     pub signature: Vec<u8>,
 }
 
